b1ee1127dab3804a22278529d6024d8849005bb7
Front-End-Lessonss <150601672+Front-End-Lessonss@users.noreply.github.com>
2024-03-04 20:56:44 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,91 @@
+// alert("hello")
+
+// document.write("hello world")
+
+// prompt("hello")
+
+// x=3;
+
+// console.log(x);
+
+// const a = document.querySelector("h1");
+
+// a.innerHTML="hello"
+
+/* kjfnsvkjngjkr
+kjdnfvkjgrn */
+
+// variable yaradilmasinin 3 merhelesi var
+
+// keys : var let const
+
+// var a =3;
+// var a = 5;
+// a= 6;
+// console.log(a)
+
+
+// let b = 3;
+// // let ile sistemde olan deyiseni ikinci defe yaratmaq olmur
+// b=6;
+// console.log(b);
+
+// const c= 2;
+// // const ile eyni deyiseni yeniden teyin etmek olmaz
+// // c=5;
+// console.log(c);
+
+// // deyisenin tipleri
+
+// const z = 3.5   //number;
+// const ad = "Leyla" //string;
+// const mentiqi =true //boolean;
+
+// const arr = ["Ali", "Leyla", "Revan"];
+// const car = {ad:"Porshe", reng:"qara"}
+
+// console.log(typeof(z))
+
+
+// const word1 = "Hello";
+// const word2 = "World";
+
+// console.log(word1+ " "+word2)
+
+// const x = 3;
+// const y =3;
+
+// console.log(x**y);
+
+// const a = 11;
+// const b = 4;
+// console.log(a%b)
+
+// = menimsetme operatorudur
+
+// const a =3;
+// const b =4;
+
+// if(a===b){
+
+//     console.log("beraberdirler")
+// }
+// else{
+//     console.log("beraber deyiller")
+// }
+
+// === tipi ile deyerinin beraber olmasini teleb edir
+// != beraber deyil demekdir
+// && and   || or
+
+
+// funksiya
+
+function Hesabla( a, b){
+
+const z = a+b;
+console.log(z)
+}
+// NaN not a number
+
+Hesabla(10, 12)
